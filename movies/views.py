9f3086277f7eb60ba258285d4c0e73c1c7b906ce
mujeeb <mujeeb.merwat@gmail.com.com>
2019-06-20 19:07:30 +0500

    Created Django movie WebApp

--- movies/views.py
+++ movies/views.py
@@ -3,10 +3,12 @@ from django.contrib import messages
 from airtable import Airtable
 import os
 
+AIRTABLE_API_KEY='keyZwX6Drbq9KeEgV'
+AIRTABLE_MOVIESTABLE_BASE_ID='appUP050v7XIBW9v8'
 
-AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID'),
+AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID', AIRTABLE_MOVIESTABLE_BASE_ID),
               'Movies',
-              api_key=os.environ.get('AIRTABLE_API_KEY'))
+              api_key=os.environ.get('AIRTABLE_API_KEY', AIRTABLE_API_KEY))
 
 # Create your views here.
 def home_page(request):
